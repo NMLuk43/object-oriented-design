3b00c6aed7168941424336588cb21fe5c5244968
NMLuk43 <97636446+NMLuk43@users.noreply.github.com>
2024-08-22 17:37:23 +0000

    new

---
+++ hw1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   /* Type your code here. */
+   cout << "Hello World!\n";
+   cout << "This is a test of my new homework file!" << endl;
+
+   bool data = true;
+
+   cout << data << endl;
+
+   return 0;
+}
